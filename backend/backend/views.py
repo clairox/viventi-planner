eae865ea0db8badfde09eb343cec8ae86f5c75f7
Claire Q <claireq620@gmail.com>
2023-11-13 13:36:49 -0500

    feat(api): add GET and DELETE methods to EventAPIView

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -17,6 +17,11 @@ class EventAPIView(APIView):
         except IntegrityError:
             return JsonResponse({'error': 'IntegrityError'}, status=400)
 
+    def get(self, request, pk):
+        event = get_object_or_404(Event, pk=pk)
+        serializer = EventSerializer(event)
+        return JsonResponse(serializer.data, status=200)
+
     def patch(self, request, pk):
         event = get_object_or_404(Event, pk=pk)
 
@@ -30,3 +35,12 @@ class EventAPIView(APIView):
             return JsonResponse(serializer.errors, status=400)
         except Exception as err:
             return JsonResponse({'error': str(err)}, status=500)
+
+    def delete(self, request, pk):
+        event = get_object_or_404(Event, pk=pk)
+
+        try:
+            event.delete()
+            return JsonResponse({'message': 'Event deleted successfully'}, status=204)
+        except:
+            return JsonResponse({'error': 'Something went wrong'}, status=500)
