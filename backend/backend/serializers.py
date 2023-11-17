4bfc5fcdb16f43db979cfa8f521df0d7926227c4
Claire Q <claireq620@gmail.com>
2023-11-17 00:05:57 -0500

    feat: add serializers for Hosts and EventRsvps

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Event
+from .models import Event, Host, EventRsvp
 from datetime import datetime
 from django.utils import timezone
 
@@ -20,3 +20,15 @@ class EventSerializer(serializers.ModelSerializer):
         validated_data['status'] = 'inactive'
 
         return Event.objects.create(**validated_data)
+
+
+class HostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Host
+        fields = '__all__'
+
+
+class EventRsvpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventRsvp
+        exclude = ['attendee_email']
