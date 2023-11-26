9dd340b74b1dfc22cd3ac14422fadaf77dee46ec
Claire Q <claireq620@gmail.com>
2023-11-26 08:55:51 -0500

    refactor: use local config

--- backend/backend/settings/local.py
+++ backend/backend/settings/local.py
@@ -1,1 +1,30 @@
 from .base import *
+
+config = useConfig('local')
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = config('AWS_ACCESS_KEY')
+
+EMAIL_HOST_PASSWORD = config('AWS_SECRET')
