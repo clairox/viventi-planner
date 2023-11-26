aafab890c9e6db77855c59757204998df2b737e4
Claire Q <claireq620@gmail.com>
2023-11-26 17:54:22 -0500

    chore: add more descriptive class names to JSX tags

--- frontend/src/pages/LinksPage.tsx
+++ frontend/src/pages/LinksPage.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet';
 import { useLocation, useParams } from 'react-router-dom';
+import '../styles/LinksPage.scss';
 
 export const LinksPage = () => {
 	const { eventSlug } = useParams();
@@ -11,21 +12,21 @@ export const LinksPage = () => {
 				<title>Verification Successful</title>
 				<meta name="description" content="Links related to your verified event." />
 			</Helmet>
-			<div>
-				<h5>Event Verified Successfully!</h5>
+			<div className="verified-details">
+				<h1 className="verified-heading">Event Verified Successfully!</h1>
 				<p>
 					Congratulations! Your event has been successfully verified. Thank you for taking this step to ensure the accuracy and legitimacy of your event on Planner. Your event is now live and visible
 					to potential attendees.
 				</p>
-				<div className="links-section" aria-labelledby="links-heading">
-					<p id="links-heading" className="links-text">
+				<div className="links-wrapper" aria-labelledby="links-heading">
+					<p id="links-heading" className="links-heading">
 						You can access your event details using the following links:
 					</p>
-					<div>
+					<div className="link-wrapper">
 						<label htmlFor="public-link">Public shareable link:</label>
 						<input id="public-link" className="link" type="text" value={`https://vvn.ti/event/${eventSlug}`} disabled />
 					</div>
-					<div>
+					<div className="link-wrapper">
 						<label htmlFor="private-link">Private link for event management:</label>
 						<input id="private-link" className="link" type="text" value={`https://vvn.ti/event/${eventSlug}?token=${token}`} disabled />
 					</div>
