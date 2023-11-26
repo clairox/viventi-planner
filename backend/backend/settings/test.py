6d733ec9c74f4b539e833880e147562eda34c7f3
Claire Q <claireq620@gmail.com>
2023-11-26 08:56:28 -0500

    refactor: change environment variables to match new names

--- backend/backend/settings/test.py
+++ backend/backend/settings/test.py
@@ -1,11 +1,15 @@
 from .base import *
 
+config = useConfig('test')
+
+# Database
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME_TEST'),
-        'USER': config('DB_USER_TEST'),
-        'PASSWORD': config('DB_PASSWORD_TEST'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'db',
         'PORT': '5432'
     }
