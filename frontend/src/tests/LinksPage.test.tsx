93e774b41d685cb8a22664ead7c3a98261f326a9
Claire Q <claireq620@gmail.com>
2023-11-26 20:27:09 -0500

    test: add test for input values not containing 'undefined'

---
+++ frontend/src/tests/LinksPage.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { LinksPage } from '../pages/LinksPage';
+
+jest.mock('../styles/LinksPage.scss', () => ({}));
+
+jest.mock('react-router-dom', () => ({
+	...jest.requireActual('react-router-dom'),
+	useParams: () => ({ eventSlug: 'mockSlug' }),
+	useLocation: () => ({ state: { token: 'mockToken' } }),
+}));
+
+test('link inputs contain valid links', () => {
+	const { getByLabelText } = render(
+		<MemoryRouter initialEntries={['/event/mockSlug/links']}>
+			<Routes>
+				<Route path="/event/:eventSlug/links" element={<LinksPage />} />
+			</Routes>
+		</MemoryRouter>
+	);
+
+	const publicLinkInput = getByLabelText('Public shareable link:') as HTMLInputElement;
+	const privateLinkInput = getByLabelText('Private link for event management:') as HTMLInputElement;
+
+	expect(publicLinkInput.value).not.toContain('undefined');
+	expect(privateLinkInput.value).not.toContain('undefined');
+});
