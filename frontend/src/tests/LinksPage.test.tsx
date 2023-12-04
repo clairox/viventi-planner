dff02e43e95af980496811691c02ec6018c4bd2e
Claire Q <claireq620@gmail.com>
2023-12-04 10:07:20 -0500

    test: move assertions to separate tests

--- frontend/src/tests/LinksPage.test.tsx
+++ frontend/src/tests/LinksPage.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import { LinksPage } from '../pages/LinksPage';
 
 jest.mock('../styles/LinksPage.scss', () => ({}));
@@ -10,18 +10,32 @@ jest.mock('react-router-dom', () => ({
 	useLocation: () => ({ state: { token: 'mockToken' } }),
 }));
 
-test('link inputs contain valid links', () => {
-	const { getByLabelText } = render(
-		<MemoryRouter initialEntries={['/event/mockSlug/links']}>
-			<Routes>
-				<Route path="/event/:eventSlug/links" element={<LinksPage />} />
-			</Routes>
-		</MemoryRouter>
-	);
+test('public link input contains valid link', () => {
+	const routes = [{ path: '/event/:eventSlug/links', element: <LinksPage /> }];
+
+	const router = createMemoryRouter(routes, {
+		initialEntries: ['/event/mockSlug/links'],
+		initialIndex: 1,
+	});
+
+	const { getByLabelText } = render(<RouterProvider router={router} />);
 
 	const publicLinkInput = getByLabelText('Public shareable link:') as HTMLInputElement;
-	const privateLinkInput = getByLabelText('Private link for event management:') as HTMLInputElement;
 
 	expect(publicLinkInput.value).toBe('https://vvn.ti/event/mockSlug');
+});
+
+test('private link input contains valid link', () => {
+	const routes = [{ path: '/event/:eventSlug/links', element: <LinksPage /> }];
+
+	const router = createMemoryRouter(routes, {
+		initialEntries: ['/event/mockSlug/links'],
+		initialIndex: 1,
+	});
+
+	const { getByLabelText } = render(<RouterProvider router={router} />);
+
+	const privateLinkInput = getByLabelText('Private link for event management:') as HTMLInputElement;
+
 	expect(privateLinkInput.value).toBe('https://vvn.ti/event/mockSlug?token=mockToken');
 });
