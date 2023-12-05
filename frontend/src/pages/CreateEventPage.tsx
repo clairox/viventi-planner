879222f16fc243ca8bffccfef4c6521579849863
Claire Q <claireq620@gmail.com>
2023-12-05 14:09:26 -0500

    feat: add title to create event page

---
+++ frontend/src/pages/CreateEventPage.tsx
@@ -0,0 +1,15 @@
+import { Helmet } from 'react-helmet';
+import { EventForm } from '../components/EventForm';
+
+// TODO put event created at end of multi step form
+export const CreateEventPage = () => {
+	return (
+		<>
+			<Helmet>
+				<title>Create an event</title>
+				<meta name="description" content="Form for event creation." />
+			</Helmet>
+			<EventForm />
+		</>
+	);
+};
