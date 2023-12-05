d2ca9da0252ccea23493950452a76865b710524a
Claire Q <claireq620@gmail.com>
2023-12-05 13:52:46 -0500

    feat: add title to event page html

---
+++ frontend/src/pages/EventPage.tsx
@@ -0,0 +1,13 @@
+import { Helmet } from 'react-helmet';
+
+export const EventPage = () => {
+	return (
+		<>
+			<Helmet>
+				<title>Create an Event</title>
+				<meta name="description" content="Form for event creation." />
+			</Helmet>
+			EventPage
+		</>
+	);
+};
