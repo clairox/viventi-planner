e15c424dc9ed9f9030a23d80ff4af84f492d3a3c
Claire Q <claireq620@gmail.com>
2023-12-07 18:45:37 -0500

    feat: create axios instance with interceptors

---
+++ frontend/src/lib/axiosInstance.tsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const baseURL = process.env.API_PATH;
+const axiosInstance = axios.create({ baseURL });
+
+axiosInstance.interceptors.response.use(
+	res => res,
+	err => {
+		console.log(err.response.status);
+		// if (err.response.status === 409) return err;
+
+		// switch (err.response.status) {
+		// 	case 400:
+		// 		window.location.href = '/400';
+		// 		break;
+		// 	case 500:
+		// 		window.location.href = '/500';
+		// 		break;
+		// 	default:
+		// 		break;
+		// }
+		// return Promise.reject(err);
+	}
+);
+
+export default axiosInstance;
