f15ed6a01162e85b3ac5fa2e48c23d268a8abfb2
Claire Q <claireq620@gmail.com>
2023-12-08 14:33:25 -0500

    chore: change class name to 'main'

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 export const MainLayout = () => {
 	return (
 		<div className="container">
-			<main className="content-area">
+			<main className="main">
 				<Outlet />
 			</main>
 		</div>
--- frontend/src/layouts/SpecialLayout.tsx
+++ frontend/src/layouts/SpecialLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 export const SpecialLayout = () => {
 	return (
 		<div className="container special">
-			<main className="content-area">
+			<main className="main">
 				<Outlet />
 			</main>
 		</div>
