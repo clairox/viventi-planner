b742a66dd348e8a8dc04b4044299526888a6a3cc
Claire Q <claireq620@gmail.com>
2023-12-08 10:12:54 -0500

    feat: add redirects to error pages on error response

--- frontend/src/lib/axiosInstance.tsx
+++ frontend/src/lib/axiosInstance.tsx
@@ -6,20 +6,18 @@ const axiosInstance = axios.create({ baseURL });
 axiosInstance.interceptors.response.use(
 	res => res,
 	err => {
-		//console.log(err.response.status);
-		// if (err.response.status === 409) return err;
+		if (err.response.status === 409) return err;
 
-		// switch (err.response.status) {
-		// 	case 400:
-		// 		window.location.href = '/400';
-		// 		break;
-		// 	case 500:
-		// 		window.location.href = '/500';
-		// 		break;
-		// 	default:
-		// 		break;
-		// }
-		// return Promise.reject(err);
+		switch (err.response.status) {
+			case 400:
+				window.location.href = '/400';
+				break;
+			case 500:
+				window.location.href = '/500';
+				break;
+			default:
+				break;
+		}
 		return Promise.reject(err);
 	}
 );
