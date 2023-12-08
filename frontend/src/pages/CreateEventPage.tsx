0a54ca54bda5d1c1c4477bd1ae9a7f72483ec341
Claire Q <claireq620@gmail.com>
2023-12-08 14:32:23 -0500

    chore: wrap jsx in section tag

--- frontend/src/pages/CreateEventPage.tsx
+++ frontend/src/pages/CreateEventPage.tsx
@@ -4,12 +4,12 @@ import { EventForm } from '../components/EventForm';
 // TODO put event created at end of multi step form
 export const CreateEventPage = () => {
 	return (
-		<>
+		<section className="event-form">
 			<Helmet>
 				<title>Create an event</title>
 				<meta name="description" content="Form for event creation." />
 			</Helmet>
 			<EventForm />
-		</>
+		</section>
 	);
 };
